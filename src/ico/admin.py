6d4c78c0aa489c92f1f9e0debd121e3927b4f08b
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2017-08-21 13:09:03 +0200

    Added admin model data to django amdin

--- src/ico/admin.py
+++ src/ico/admin.py
@@ -3,3 +3,9 @@ from ico.models import *
 
 admin.site.register(User)
 admin.site.register(Company)
+admin.site.register(Currency)
+admin.site.register(Ico)
+admin.site.register(Phase)
+admin.site.register(Rate)
+admin.site.register(Quote)
+admin.site.register(Purchase)
